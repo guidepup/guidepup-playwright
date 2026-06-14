9336cf529276b32412334ee342bae3af67477d69
cmorten <craig.morten@hotmail.co.uk>
2026-06-14 21:20:22 +0100

    feat: improve on the VO `navigateToWebContent` by using window spots

--- src/voiceOverTest.ts
+++ src/voiceOverTest.ts
@@ -102,27 +102,21 @@ export const voiceOverTest = test.extend<{
         // Ensure the document is ready and focused.
         await page.bringToFront();
         await page.locator("body").waitFor();
-        await page.locator("body").focus();
-        await page.locator("body").click();
-        await page.locator("body").blur();
 
-        // Try to navigate into web content.
+        // Open the web item rotor defaulting to window spots.
+        await voiceOverPlaywright.perform(
+          voiceOverPlaywright.keyboardCommands.openWebItemRotor,
+        );
+
+        // Filter by "content" - currently web content spots for all browsers
+        // are prefixed by "Content -".
+        await voiceOverPlaywright.type("content");
+
+        // Select the web content window spot.
+        await voiceOverPlaywright.perform({ keyCode: MacOSKeyCodes.Enter });
+
+        // Navigate into web content.
         await voiceOverPlaywright.interact();
-
-        // Cancel auto navigation
-        await voiceOverPlaywright.perform({ keyCode: MacOSKeyCodes.Control });
-
-        // Series of find previous commands to escape accidental interaction
-        // with sub-content of web content area.
-        await voiceOverPlaywright.perform(
-          voiceOverPlaywright.keyboardCommands.findPreviousHeading,
-        );
-        await voiceOverPlaywright.perform(
-          voiceOverPlaywright.keyboardCommands.findPreviousGraphic,
-        );
-        await voiceOverPlaywright.perform(
-          voiceOverPlaywright.keyboardCommands.findPreviousPlainText,
-        );
 
         // Navigate to the beginning of the web content.
         await voiceOverPlaywright.perform(
